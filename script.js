ca90e00bc87fa1cf72e06f5607158589929f9440
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-18 17:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function myfunction() {
+	const dropdown = document.getElementById("colorSelect");
+      const selectedIndex = dropdown.selectedIndex;
+      dropdown.remove(selectedIndex);
+}
